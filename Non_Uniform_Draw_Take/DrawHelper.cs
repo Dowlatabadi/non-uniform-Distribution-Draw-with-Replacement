4fc157691cda34a1004d3c1d5b3827bd40bdb04d
mehrdad8 <43187348+mehrdad8@users.noreply.github.com>
2020-03-19 04:40:09 +0330

    first

---
+++ Non_Uniform_Draw_Take/DrawHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Non_Uniform_Draw_Take
+{
+    public static class DrawHelper
+    {
+        public static IEnumerable<T> Take_Uniform<T>(this IEnumerable<T> Items, int Take_number)
+        {
+            Random rnd = new Random();
+            return Items.OrderBy(x => rnd.Next()).Take(Take_number);
+        }
+        public static IEnumerable<T> Take_Non_Uniform<T>(this IEnumerable<T> Items,List<double> Weights, int Take_number,bool Replacement=true)
+        {
+            //TODO replacement false
+            if (Weights.Count() != Items.Count())
+                throw new Exception($"weights and items should be equal in length");
+            var sum = Weights.Sum();
+            var Normalized_Weights = Weights.Select(x => x / sum).ToArray();
+            Random rnd = new Random();
+            for (int j = 0; j < Take_number; j++)
+            {
+                var current_rnd = rnd.NextDouble();
+                for (int i = 0; i < Items.Count(); i++)
+                {
+                    if (current_rnd <= Normalized_Weights[i])
+                    {
+                        yield return Items.ElementAt(i);
+                        break;
+                    }
+                    current_rnd -= Normalized_Weights[i];
+                }
+            }
+        }
+    }
+}
---
+++ Non_Uniform_Draw_Take/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Non_Uniform_Draw_Take
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var selected = new List<int>() { 1, 4, 8 }.Take_Non_Uniform(new List<double>() { 1d, 4d, 8d }, 100);
+            selected.ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
