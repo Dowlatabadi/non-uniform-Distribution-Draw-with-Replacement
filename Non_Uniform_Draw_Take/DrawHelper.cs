7a07126808eb0b320e421bfbfc0ca78dfc40df83
mehrdad8 <43187348+mehrdad8@users.noreply.github.com>
2020-03-19 05:12:55 +0330

    replacement fixed

--- Non_Uniform_Draw_Take/DrawHelper.cs
+++ Non_Uniform_Draw_Take/DrawHelper.cs
@@ -14,20 +14,28 @@ namespace Non_Uniform_Draw_Take
         }
         public static IEnumerable<T> Take_Non_Uniform<T>(this IEnumerable<T> Items,List<double> Weights, int Take_number,bool Replacement=true)
         {
+            HashSet<int> drawn = new HashSet<int>();
             //TODO replacement false
             if (Weights.Count() != Items.Count())
                 throw new Exception($"weights and items should be equal in length");
+            if (Replacement==false && Take_number>Items.Count())
+                throw new Exception($"theres a few items can't take many items");
             var sum = Weights.Sum();
             var Normalized_Weights = Weights.Select(x => x / sum).ToArray();
             Random rnd = new Random();
+            double start = 0;
             for (int j = 0; j < Take_number; j++)
             {
-                var current_rnd = rnd.NextDouble();
+                var current_rnd = rnd.NextDouble()*(1-start);
                 for (int i = 0; i < Items.Count(); i++)
                 {
+                    if (drawn.Contains(i))
+                        continue;
                     if (current_rnd <= Normalized_Weights[i])
                     {
                         yield return Items.ElementAt(i);
+                        start += Normalized_Weights[i];
+                        drawn.Add(i);
                         break;
                     }
                     current_rnd -= Normalized_Weights[i];
--- Non_Uniform_Draw_Take/Program.cs
+++ Non_Uniform_Draw_Take/Program.cs
@@ -8,7 +8,7 @@ namespace Non_Uniform_Draw_Take
     {
         static void Main(string[] args)
         {
-            var selected = new List<int>() { 1, 4, 8 }.Take_Non_Uniform(new List<double>() { 1d, 4d, 8d }, 100);
+            var selected = new List<int>() { 1,2, 4, 8,16,32,64,128 ,256,512,1024}.Take_Non_Uniform(new List<double>() { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 },5,Replacement:false);
             selected.ToList().ForEach(x => Console.WriteLine(x));
         }
     }
